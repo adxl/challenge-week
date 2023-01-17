944f8f483c01a5f35af7a83532ebe96ebb7d8c48
ZERRAI Sami <sami.zerrai@gmail.com>
2023-01-17 23:01:08 +0100

    add tailwind && flowbite

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,12 +3,12 @@ import Header from "./components/Header.vue";
 </script>
 
 <template>
-    <div className="h-full">
-        <Header />
-        <main className="flex items-center justify-center h-screen">
-            <router-view />
-        </main>
-    </div>
+  <div className="h-full">
+    <Header />
+    <main className="flex items-center justify-center h-screen">
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <style scoped></style>
