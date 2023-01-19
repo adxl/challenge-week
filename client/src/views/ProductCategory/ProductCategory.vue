24830c46e6f2c97a59f880ce558ed85447e99f9b
ZERRAI Sami <sami.zerrai@gmail.com>
2023-01-19 10:10:10 +0100

    add remove dark to form

--- client/src/views/ProductCategory/ProductCategory.vue
+++ client/src/views/ProductCategory/ProductCategory.vue
@@ -4,7 +4,6 @@ import {
   addProductCategory,
   updateProductCategory,
 } from "@/api/productCategory";
-import { isIntegerKey } from "@vue/shared";
 import { Form, Field, ErrorMessage } from "vee-validate";
 import { useRouter } from "vue-router";
 import { onMounted, reactive } from "vue";
@@ -36,7 +35,7 @@ function handleRegister(values) {
   if (id) {
     updateProductCategory(id, { ...values })
       .then(({ data }) => {
-        route.push({ name: "product-types" });
+        route.push({ name: "product-categorys" });
       })
       .catch((error) => {
         console.log(error);
@@ -45,7 +44,7 @@ function handleRegister(values) {
   } else {
     addProductCategory({ id, ...values })
       .then(({ data }) => {
-        route.push({ name: "product-types" });
+        route.push({ name: "product-categorys" });
       })
       .catch((error) => {
         console.log(error);
@@ -57,7 +56,7 @@ function handleRegister(values) {
 <template>
   <div class="container mx-auto px-4 sm:px-8">
     <h3
-      class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+      class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl"
     >
       {{ id ? "Modification" : "Création" }} d'un type de produit
     </h3>
@@ -67,24 +66,22 @@ function handleRegister(values) {
       :validation-schema="simpleSchema"
     >
       <div class="mb-6">
-        <label
-          for="name"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        <label for="name" class="block mb-2 text-sm font-medium text-gray-900"
           >Nom de la catégorie</label
         >
         <Field
           name="name"
-          class="shadow-sm mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+          class="shadow-sm mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         />
         <ErrorMessage
-          class="p-1 mb-1 text-sm text-red-700 borderrounded-lg dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+          class="p-1 mb-1 text-sm text-red-700 borderrounded-lg"
           name="name"
         >
         </ErrorMessage>
       </div>
       <button
         type="submit"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
       >
         Ajouter une catégorie de produit
       </button>
