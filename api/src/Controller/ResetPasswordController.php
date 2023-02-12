455047a131843426ea4e28619dd6535986b86cd3
Adel Sen <adelsenhadjii@gmail.com>
2023-02-12 19:42:41 +0100

    Fix: fix mailer autowireing

--- api/src/Controller/ResetPasswordController.php
+++ api/src/Controller/ResetPasswordController.php
@@ -17,79 +17,89 @@ use Symfony\Component\Mime\Email;
 #[AsController]
 class ResetPasswordController extends AbstractController
 {
-    public function __construct(
-        private RequestStack $requestStack,
-        private ManagerRegistry $managerRegistry
-    ) {}
+  private  $requestStack;
+  private  $managerRegistry;
+  private $mailerInterface;
 
-    // TEMPLATE 7
-    #[Route(
-        name: 'reset_password',
-        path: '/users/reset-password/{token}',
-        methods: ['PATCH'],
-        defaults: [
-            '_api_operation_name' => '_api_/users/reset-password/{token}',
-        ],
-    )]
-    public function __invoke(string $token, UserPasswordHasherInterface $passwordHasher): JsonResponse
-    {
-        // TODO : Secure if not password in body
-        $password = json_decode($this->requestStack->getCurrentRequest()->getContent())->password;
+  public function __construct(
+    RequestStack $requestStack,
+    ManagerRegistry $managerRegistry,
+    MailerInterface $mailerInterface
+  ) {
+    $this->requestStack = $requestStack;
+    $this->managerRegistry = $managerRegistry;
+    $this->mailerInterface = $mailerInterface;
+  }
 
-        /** @var User $user */
-        if (!$user = $this->managerRegistry->getRepository(User::class)->findOneBy(['token' => $token])) {
-            throw $this->createNotFoundException();
-        }
-      
-        if ($user->getStatus() === "INACTIVE") {
-            $user->setStatus("ACTIVE");
-        }
+  // TEMPLATE 7
+  #[Route(
+    name: 'reset_password',
+    path: '/users/reset-password/{token}',
+    methods: ['PATCH'],
+    defaults: [
+      '_api_operation_name' => '_api_/users/reset-password/{token}',
+    ],
+  )]
+  public function __invoke(string $token, UserPasswordHasherInterface $passwordHasher): JsonResponse
+  {
+    // TODO : Secure if not password in body
+    $password = json_decode($this->requestStack->getCurrentRequest()->getContent())->password;
 
-        $user->setPassword($passwordHasher->hashPassword($user, $password));
-        $user->setToken(null);
-        $this->managerRegistry->getManager()->flush();
-
-        return $this->json([
-          "code" => 200,
-          "message" => "Password changed"
-        ]);
+    /** @var User $user */
+    if (!$user = $this->managerRegistry->getRepository(User::class)->findOneBy(['token' => $token])) {
+      throw $this->createNotFoundException();
     }
 
-    #[Route(
-        name: 'reset_password_email',
-        path: '/users/reset-password',
-        methods: ['POST'],
-        defaults: [
-            '_api_operation_name' => '_api_/users/reset-password',
-        ],
-    )]
-    public function sendRstPwdMail(ManagerRegistry $doctrine, MailerInterface $mailer): JsonResponse
-    {
-        // TODO : Secure if not email in body
-        $email = json_decode($this->requestStack->getCurrentRequest()->getContent())->email;
-
-        if (!$user = $this->managerRegistry->getRepository(User::class)->findOneBy(['email' => $email])) {
-            throw $this->createNotFoundException();
-        }
-
-        $em = $doctrine->getManager();
-        $token = TokenGeneration::generateToken();
-        $user->setToken($token);
-        $em->flush();
-
-        $url = $_ENV["APP_URL"] . '/reset-password/' . $token;
-
-        $email = (new Email())
-            ->from('supp.myschool@outlook.fr')
-            ->to($user->getEmail())
-            ->subject('Réinitialisation de votre mot de passe')
-            ->html('Pour changer votre mot de passe, veuillez cliquer sur le lien suivant : <a href="' . $url . '">Réinitiliser mon mot de passe</a>');
-
-        $mailer->send($email);
-
-        return $this->json([
-          "code" => 200,
-          "message" => "Email sent"
-        ]);
+    if ($user->getStatus() === "INACTIVE") {
+      $user->setStatus("ACTIVE");
     }
+
+    $user->setPassword($passwordHasher->hashPassword($user, $password));
+    $user->setToken(null);
+    $this->managerRegistry->getManager()->flush();
+
+    return $this->json([
+      "code" => 200,
+      "message" => "Password changed"
+    ]);
+  }
+
+  #[Route(
+    name: 'reset_password_email',
+    path: '/users/reset-password',
+    methods: ['POST'],
+    defaults: [
+      '_api_operation_name' => '_api_/users/reset-password',
+    ],
+  )]
+  public function sendRstPwdMail(ManagerRegistry $doctrine): JsonResponse
+  {
+    // TODO : Secure if not email in body
+    $email = json_decode($this->requestStack->getCurrentRequest()->getContent())->email;
+
+    if (!$user = $this->managerRegistry->getRepository(User::class)->findOneBy(['email' => $email])) {
+      throw $this->createNotFoundException();
+    }
+
+    $em = $doctrine->getManager();
+    $token = TokenGeneration::generateToken();
+    $user->setToken($token);
+    $em->flush();
+
+    $url = $_ENV["APP_URL"] . '/reset-password/' . $token;
+
+    $email = (new Email())
+      ->from('supp.myschool@outlook.fr')
+      ->to($user->getEmail())
+      ->subject('Réinitialisation de votre mot de passe')
+      ->html('Pour changer votre mot de passe, veuillez cliquer sur le lien suivant : <a href="' . $url . '">Réinitiliser mon mot de passe</a>');
+
+    $this->mailerInterface->send($email);
+
+    return $this->json([
+      "code" => 200,
+      "message" => "Email sent"
+    ]);
+  }
 }
+
